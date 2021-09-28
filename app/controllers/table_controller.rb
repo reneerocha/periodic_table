78bad7e8276983a58ba2944d6a3437c75e2dabb7
Renee Rocha <reneerocha.pi@gmail.com>
2021-09-28 11:50:33 -0300

    create index

--- app/controllers/table_controller.rb
+++ app/controllers/table_controller.rb
@@ -1,6 +1,10 @@
 class TableController < ApplicationController
   def index
-    file = File.read("db/data.json")
-    @elements = JSON.parse(file)
+    @elements = Element.all
   end
+
+  def show
+    
+  end
+
 end
